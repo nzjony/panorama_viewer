111ef7ea0ed84f97a77a83de1f7c34934ee33752
Jonathan Stichbury <2533428+nzjony@users.noreply.github.com>
2024-06-22 14:38:37 +0200

    Place sphere near face

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
+  90,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
@@ -12,12 +12,18 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.SphereGeometry(1, 16, 16)
+const geometry = new THREE.SphereGeometry(1, 16, 16);
 const texture = new THREE.TextureLoader().load('/panorama.jpg');
 texture.colorSpace = THREE.SRGBColorSpace;
 const material = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, map: texture});
+const pointMaterial = new THREE.MeshBasicMaterial({color: "red"});
+pointMaterial.depthTest = false;
 const cube = new THREE.Mesh(geometry, material);
+cube.scale.setScalar(100);
+const smallPoint = new THREE.Mesh(geometry, pointMaterial);
+smallPoint.scale.setScalar(0.01);
 scene.add(cube);
+scene.add(smallPoint);
 
 let startDrag = new THREE.Vector2();
 window.addEventListener('pointerdown', (event) => {
@@ -39,10 +45,30 @@ const rotateSphere = (xOffset: number, yOffset: number) => {
 }
 
 window.addEventListener('pointermove', (event) => {
+  const diffX = event.clientX - startDrag.x;
+  const diffY = event.clientY - startDrag.y;
   if (event.buttons === 1) {
-    rotateSphere(event.clientX - startDrag.x, event.clientY - startDrag.y);
+    rotateSphere(diffX, diffY);
     startDrag.x = event.clientX;
     startDrag.y = event.clientY;
+  }
+
+  const ndc = new THREE.Vector2(((event.clientX - (window.innerWidth / 2)) / (window.innerWidth / 2)),
+    (-event.clientY - (window.innerHeight / 2)) / (window.innerHeight / 2) + 2);
+
+    console.log(ndc);
+
+  const raycaster = new THREE.Raycaster();
+  raycaster.setFromCamera(ndc, camera);
+
+  const intersects : THREE.Intersection[] = [];
+  cube.raycast(raycaster, intersects);
+
+  if (intersects.length > 0) {
+    const point = intersects[0];
+    const pointLocation = point.point;
+    pointLocation.normalize();
+    smallPoint.position.copy(pointLocation);
   }
 });
 
